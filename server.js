abca7e459bac9c562c1d8b8ddd409a798e861071
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-10-21 08:44:24 -0500

    feat :sparkles: Setting up https server with ssl certificate

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
 const express = require("express");
 const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 const db = require("./server/db/db");
 const app = express();
 
-app.use(cors(
-    {
-        origin: "http://localhost:3000",
-        credentials: true
-    }
-));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 db.getInstace();
 
+// Cargar certificado y clave privada
+const privateKey = fs.readFileSync("./private.key");
+const certificate = fs.readFileSync("./certificate.crt");
+
+// Crear servidor HTTPS
+const httpsServer = https.createServer(
+  {
+    key: privateKey,
+    cert: certificate,
+  },
+  app
+);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+httpsServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   root: 'client',
   server: {
-    port: 3000
+    port: 3000,
+    open: true,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, './private.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, './certificate.crt')),
+    },
   }
 })
